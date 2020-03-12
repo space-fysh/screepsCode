93f347dd2e6676771114ad909f9f640cb06926c5
Brett Thomas <thoma22bt@gmail.com>
2020-03-11 20:00:45 -0600

    added comment

--- class.bro.js
+++ class.bro.js
@@ -1,37 +1,37 @@
 var classBro = {
-    
+
     costSmall: 200,
     costMedium: 300,
     costLarge: 450,
     minBros: 10,
-    
-    
-    
+
+
+    //jh;fda
     spawnin: function(size) {
         var energyAvaliable = 0;
         var energyCap = 0;
-        
+
         for(let roomName in Game.rooms) { //{console.log(Game.rooms[roomName].energyAvailable)}
-        
+
             energyAvaliable = Game.rooms[roomName].energyAvailable + energyAvaliable
             energyCap = Game.rooms[roomName].energyCapacityAvailable + energyCap
             console.log(roomName)
         }
-        
+
              for(var name in Memory.creeps) {
                 if(!Game.creeps[name]) {
                     delete Memory.creeps[name];
                     //console.log('Clearing non-existing creep memory:', name);
                 }
             }
-        
+
         const numBros =  (_.filter(Game.creeps, (creep) => creep.memory.class == 'bro')).length;
         console.log(numBros)
-        
-         if (numBros < classBro.minBros){   
+
+         if (numBros < classBro.minBros){
              //console.log('trying to spawn a bro')
              //console.log('Energy avaliable: ' + energyAvaliable)
-             
+
             //bro class can be harvesters, repairers, upgraders, or builders. Change job depending on need
             if((energyAvaliable >= classBro.costLarge) || (size== 'l')){
                 classBro.spawnLarge();
@@ -43,13 +43,13 @@ var classBro = {
                 classBro.spawnSmall();
             }
          }
-            
+
     },
-    
+
     spawnLarge: function() {
             var newName = 'bro' + Game.time;
             console.log('Spawning new bro: ' + newName);
-            if(!Game.spawns['spo'].spawnCreep([WORK,WORK,WORK,CARRY,MOVE,MOVE], newName, 
+            if(!Game.spawns['spo'].spawnCreep([WORK,WORK,WORK,CARRY,MOVE,MOVE], newName,
                 {memory: {role: '', class: 'bro', working: false}})){
                     console.log('spawning a large bro')
         }
@@ -59,27 +59,27 @@ var classBro = {
     },
     spawnMedium: function() {
             var newName = 'bro' + Game.time;
-            
+
         console.log('Spawning new bro: ' + newName);
-            if (!Game.spawns['spo'].spawnCreep([WORK,WORK,CARRY,MOVE], newName, 
+            if (!Game.spawns['spo'].spawnCreep([WORK,WORK,CARRY,MOVE], newName,
                 {memory: {role: '', class: 'bro', working: false}})){
         }
         else {
             console.log('Too low energy to spawn medium bro')
         }
-        
+
     },
     spawnSmall: function() {
             var newName = 'bro' + Game.time;
             console.log('Spawning new bro: ' + newName);
-            if (!Game.spawns['spo'].spawnCreep([WORK,CARRY,MOVE], newName, 
+            if (!Game.spawns['spo'].spawnCreep([WORK,CARRY,MOVE], newName,
                 {memory: {role: '', class: 'bro', working: false}})) {
         }
         else {
             console.log('Too low energy to spawn small bro')
         }
     }
-    
+
 };
 
 module.exports = classBro;
