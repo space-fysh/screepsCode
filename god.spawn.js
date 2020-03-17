ef518c00af16b9a35d2f2ed74fa6244d100a24fa
Brett Thomas <thoma22bt@gmail.com>
2020-03-16 20:36:02 -0600

    trying to fix rooms

--- god.spawn.js
+++ god.spawn.js
@@ -4,25 +4,28 @@ var movers = require('role.mover')
 
 var godSpawn = {
   spawnMiners: function() {
-    for(spawn in Game.spawns){
-      console.log('spawning miners for Spawn: ' + spawn.room)
+    for(let spawn in Game.spawns){
+      
+      let room = Game.spawns[spawn].room
+
+      console.log('spawning miners for Spawn: ' + spawn + " in room: " + room)
       // var harvesters = (_.filter(Game.creeps, (creep) => creep.memory.role == 'harvester')).length;
       // var upgraders = (_.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader')).length;
       // var builders = (_.filter(Game.creeps, (creep) => creep.memory.role == 'builder')).length;
       // var repairers = (_.filter(Game.creeps, (creep) => creep.memory.role == 'repairer')).length;
       var miners = (_.filter(Game.creeps, (creep) => creep.memory.role == 'miner')).length;
       var movers = (_.filter(Game.creeps, (creep) => creep.memory.role == 'miner')).length;
-      var energyAvaliable = Game.rooms[spawn.room].energyAvailable
+      var energyAvaliable = Game.rooms[room].energyAvailable
       //var noJobs = _.filter(Game.creeps, (creep) => creep.memory.role == '' || creep.memory.role == 'general');
 
 
 
       //check number of miners in room, produce one for each source
       if (miners < 2){
-        dedHarvester.spawn(Game.rooms[spawn.room]);
+        dedHarvester.spawn(Game.rooms[room]);
       }
       if (movers < 3){
-        movers.spawn(Game.rooms[spawn.room]);
+        movers.spawn(Game.rooms[room]);
       }
     }
   }
